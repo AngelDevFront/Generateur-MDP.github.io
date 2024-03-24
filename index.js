e88f44fdb5b02cb013a9eee23e53cb368e31861a
david fontaine <devilangel6089@gmail.comgit config --global user.name AngelDevil96>
2024-03-24 10:27:54 +0100

    ajout de commentaire

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const passwordOutput = document.getElementById("password-output");
 function generatePassword() {
   let data = []; // cree un tableau
   let password = "";
+  // data.push(... un element)serre toujours a ajouter un element d'un tableau a un autre les ... peuvent aussi etre utiliser avec d'autre objet les ... s'appelle un operateur de propagation
+  if (lowercase.checked) data.push(...dataLowercase); //va permettre d'ajouter dans le tableau les minuscule si la case minuscule est cochée
+  if (uppercase.checked) data.push(...dataUppercase); //va permettre d'ajouter dans le tableau les majuscule si la case majuscule est cochée
+  if (numbers.checked) data.push(...dataNumbers); //va permettre d'ajouter dans le tableau des nombres si la case nombre est cochée
+  if (symbols.checked) data.push(...dataSymbols); //va permettre d'ajouter dans le tableau des symboles si la case symboles est cochée
 
-  if (lowercase.checked) data.push(...dataLowercase);
-  if (uppercase.checked) data.push(...dataUppercase);
-  if (numbers.checked) data.push(...dataNumbers);
-  if (symbols.checked) data.push(...dataSymbols);
-
+  // creer un message d'alert pour l'utilisatur si le resultat est 0 il va dire que l'utilisateur doit séléction des critères
   if (data.length === 0) {
     alert("Veuillez sélectionner des critères");
     return;
@@ -27,6 +28,7 @@ function generatePassword() {
   passwordOutput.value = password;
   passwordOutput.select();
 
+  //permet de copier le mot de passe généré dans le presse papier afin de la mettre autre part
   navigator.clipboard.writeText(passwordOutput.value);
 
   generateButton.textContent = "Copié !";
